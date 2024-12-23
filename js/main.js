60e99cde869d200ce51c1a1f191f98c4f511dbc0
abhishek-baliyan-dev <abhishek.baliyan.dev@gmail.com>
2024-12-23 17:51:04 +0530

    Added pop up

--- js/main.js
+++ js/main.js
@@ -92,3 +92,16 @@
     
 })(jQuery);
 
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
